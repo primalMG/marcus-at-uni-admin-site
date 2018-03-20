223b9ab2f55bb3aaa8dfe47d1ae7f9f6d0190145
Marcus Gardner <danielmarcus05@hotmail.com>
2018-03-20 14:17:02 +0000

    we making progress

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+var app = require('express')();
+var http = require('http').Server(app);
 var admin = require('firebase-admin');
+var io = require('socket.io')(http);
+var serviceAccount = require('./Marcus at uni service account.json');
 
-var serviceAccount = require('./Marcus-at-uni-service-account.json');
+app.get('/',function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+http.listen(2000,function(){
+    console.log('listening on 2000');
+});
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -8,3 +19,29 @@ admin.initializeApp({
 });
 
 var ref = admin.database().ref
+
+var socketList = {};
+
+var admins = {
+    "marcus":"pass",
+}
+
+var passwordValidation = function(data){
+    return admins[data.name] === data.pass;
+}
+
+//serverside goodness
+io.on('connection', function(socket){
+    socket.id = Math.random();
+    socketList[socket.id] = socket;
+
+    socket.on('join', function(data){
+        if(passwordValidation(data)){
+            socket.emit('loginResponse',{success:true});
+        } else {
+            socket.emit('loginResponse',{success:false});
+        }
+    });
+
+
+});
