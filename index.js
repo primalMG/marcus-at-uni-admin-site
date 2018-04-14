7961c114222f2d084bc60559b4eba1ac44bb851c
Marcus Gardner <danielmarcus05@hotmail.com>
2018-04-14 19:37:42 +0100

    quick commit before work

--- index.js
+++ index.js
@@ -8,8 +8,69 @@ var pass = document.getElementById('pass');
 
 //somehow gotta figure out how to read the list and put it into a table?
 var ref = database.ref('Recipe');
-ref.on('child_added', function(data){
-    console.log(data.val().name, data.val().recipeID);
+var ingredientsRef = database.ref().child('Recipe').child('Ingredients');
 
-    var recipeName = (data.val().name, data.val().recipeID);
+const recipeList = document.getElementById('recipeList');
+
+ref.on('child_added', snap => {
+    console.log(snap);
+    let recipe = snap.val();
+    let $li = document.createElement('li');
+    $li.innerHTML = recipe.name;
+    $li.setAttribute('childKey', snap.key);
+    $li.addEventListener('click', recipeClicked)
+    recipeList.appendChild($li);
+
 });
+
+function recipeClicked(e){
+    var recipeID = e.target.getAttribute('childKey');
+    const recipeRef = database.ref('Recipe/' + recipeID );
+
+    const recipeDetail = document.getElementById('RecipeDetails');
+    recipeDetail.innerHTML = ""
+
+    recipeRef.on('child_added', snap => {
+        var $p = document.createElement("p");
+        $p.innerHTML = snap.key + " - " + snap.val()
+        recipeDetail.append($p);
+    });
+}
+
+// ref.on('child_added', function(data){
+//     console.log(data.val().name, data.val().Ingredients);
+//     let recipe = data.val();
+//     let $li = document.createElement('li');
+//     $li.innerHTML = recipe.name;
+//     $li.setAttribute('childKey', data.key);
+//     $li.addEventListener('click', recipeClicked)
+//     recipeList.appendChild($li);
+// });
+
+// function recipeClicked(e){
+//     var recipeID = e.target.getAttribute('childKey');
+//     const recipeRef = database.ref('Recipe' + recipeID );
+
+//     const recipeDetail = document.getElementById('RecipeDetails');
+//     recipeDetail.innerHTML = ""
+
+//     ref.on('child_added', snap => {
+//         var $p = document.createElement("p");
+//         $p.innerHTML = snap.val().Ingredients;
+//         recipeDetail.append($p);
+//     });
+// }   
+
+// ref.once('value', function(snapshot){
+//     snapshot.forEach(function(childSnap){
+//         console.log(childSnap.val());
+
+//     let recipe = childSnap.val();
+//     let $li = document.createElement('li');
+//     $li.innerHTML = recipe;
+//     $li.setAttribute('childKey', childSnap.key);
+//    // $li.addEventListener('click', recipeClicked)
+//     recipeList.appendChild($li);
+
+//     });
+// });
