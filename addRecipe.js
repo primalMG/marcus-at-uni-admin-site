64f3eaf1f0d4ede687d472e64f7b3717b92530ca
Marcus Gardner <danielmarcus05@hotmail.com>
2018-04-14 09:58:32 +0100

    add recipe working without error handling + started working on write

--- addRecipe.js
+++ addRecipe.js
@@ -20,45 +20,50 @@
     var stepFour = document.getElementById('stepFour');
 
     var img = document.getElementById('img');
+    var imgs;
 
     img.addEventListener('change', function(e){
-        var imgs = e.target.files[0];
-
-        var storageRef = firebase.storage().ref('Food_pics/' + imgs.name);
-
-        var upload = storageRef.put(imgs);
+        imgs = e.target.files[0];
     });
         
 
     
     function addRecipe(){
-        var newRecipeRef = recipeRef.push().key;
+        //uploading the image to storage.
+        var storageRef = firebase.storage().ref('Food_pics/' + imgs.name);
+        var upload = storageRef.put(imgs).then(function(snapshot) {
+            var downloadURL = snapshot.downloadURL;
+            var newRecipeRef = recipeRef.push().key;
 
-       var recipeData = {
-            name: names.value,
-            recipeID: newRecipeRef,    
-            price: price,
-            //img: imageURL,
-            Ingredients : {
-                Ing1: ingOne.value,
-                Ing2: ingTwo.value,
-                Ing3: ingThree.value,
-                Ing4: ingFour.value,
-            },
-            steps: {
-                step1: stepOne.value,
-                step2: stepTwo.value,
-                step3: stepThree.value,
-                step4: stepFour.value,
-            }
-        };
-
-           
-
+            //populating the database with values from the DOMS 
+            var recipeData = {
+                name: names.value,
+                recipeID: newRecipeRef,    
+                price: price,
+                img: downloadURL,
+                Ingredients : {
+                    Ing1: ingOne.value,
+                    Ing2: ingTwo.value,
+                    Ing3: ingThree.value,
+                    Ing4: ingFour.value,
+                },
+                steps: {
+                    step1: stepOne.value,
+                    step2: stepTwo.value,
+                    step3: stepThree.value,
+                    step4: stepFour.value,
+                }
+            };
+         
+        //uploading the recipe to the database.    
         var newRecipe = {};
         newRecipe['/Recipe/' + newRecipeRef] = recipeData;
 
         return firebase.database().ref().update(newRecipe);
+
+        });
+
+      
     }
 
     
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+var database = firebase.database();
+
 //Login variables
 var login = document.getElementById('login');
 var email = document.getElementById('email');
 var pass = document.getElementById('pass');
+
+
+//somehow gotta figure out how to read the list and put it into a table?
+var ref = database.ref('Recipe');
+ref.on('child_added', function(data){
+    console.log(data.val().name, data.val().recipeID);
+
+    var recipeName = (data.val().name, data.val().recipeID);
+});
