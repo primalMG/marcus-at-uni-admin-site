f9a227f231cc5e9786d3586c8e1ee009111b352a
Marcus Gardner <danielmarcus05@hotmail.com>
2018-04-15 16:07:01 +0100

    delete working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var database = firebase.database();
+var database = firebase.database().ref();
 
 //Login variables
 var login = document.getElementById('login');
@@ -7,70 +7,84 @@ var pass = document.getElementById('pass');
 
 
 //somehow gotta figure out how to read the list and put it into a table?
-var ref = database.ref('Recipe');
-var ingredientsRef = database.ref().child('Recipe').child('Ingredients');
+var recipeRef = database.child('Recipe');
 
 const recipeList = document.getElementById('recipeList');
 
-ref.on('child_added', snap => {
-    console.log(snap);
-    let recipe = snap.val();
-    let $li = document.createElement('li');
-    $li.innerHTML = recipe.name;
-    $li.setAttribute('childKey', snap.key);
-    $li.addEventListener('click', recipeClicked)
-    recipeList.appendChild($li);
+recipeRef.on('value', snap => {
+
+    recipeList.innerHTML = ""
+
+    snap.forEach(childSnap => {
+        let key = childSnap.key,
+            value = childSnap.val();
+        let $li = document.createElement('li');
+
+        $li.innerHTML = value.name;
+       
+
+
+        //Edit a recipe
+        let btnEdit = document.createElement('span');
+        btnEdit.class = 'editRecipe';
+        btnEdit.innerHTML = ' Edit';
+        btnEdit.setAttribute('childKey', key);
+        //btnEdit.addEventListener('click', btnEditRecipe);
+        $li.append(btnEdit)
+    
+        //Deleting a recipe
+        let btnDelete = document.createElement("span");
+        btnDelete.class = 'deleteRecipe';
+        btnDelete.innerHTML = ' Delete';
+        btnDelete.setAttribute('childKey', key);
+        btnDelete.addEventListener('click', btnDeleteClicked);
+
+        $li.append(btnDelete)
+        $li.setAttribute('keyy', key);
+        $li.addEventListener('click', recipeClicked)
+        recipeList.append($li);
+    });
+    //console.log(snap);
+
+    
 
 });
 
 function recipeClicked(e){
-    var recipeID = e.target.getAttribute('childKey');
-    const recipeRef = database.ref('Recipe/' + recipeID );
+    var recipeID = e.target.getAttribute('keyy');
+    
+    const selectedRecipe = database.child('Recipe/' + recipeID );
+
 
     const recipeDetail = document.getElementById('RecipeDetails');
-    recipeDetail.innerHTML = ""
+   
 
-    recipeRef.on('child_added', snap => {
-        var $p = document.createElement("p");
-        $p.innerHTML = snap.key + " - " + snap.val()
-        recipeDetail.append($p);
+
+    selectedRecipe.on('value', snap => {
+        recipeDetail.innerHTML = ""
+        snap.forEach(childSnap => {
+            console.log(childSnap)
+            var $p = document.createElement("p");
+            $p.innerHTML = childSnap.key + " - " + childSnap.val();
+            recipeDetail.append($p)
+        });
     });
 }
 
-// ref.on('child_added', function(data){
-//     console.log(data.val().name, data.val().Ingredients);
-//     let recipe = data.val();
-//     let $li = document.createElement('li');
-//     $li.innerHTML = recipe.name;
-//     $li.setAttribute('childKey', data.key);
-//     $li.addEventListener('click', recipeClicked)
-//     recipeList.appendChild($li);
-// });
 
-// function recipeClicked(e){
-//     var recipeID = e.target.getAttribute('childKey');
-//     const recipeRef = database.ref('Recipe' + recipeID );
 
-//     const recipeDetail = document.getElementById('RecipeDetails');
-//     recipeDetail.innerHTML = ""
+//update a recipe -- gonna be a challenge
 
-//     ref.on('child_added', snap => {
-//         var $p = document.createElement("p");
-//         $p.innerHTML = snap.val().Ingredients;
-//         recipeDetail.append($p);
-//     });
-// }   
 
-// ref.once('value', function(snapshot){
-//     snapshot.forEach(function(childSnap){
-//         console.log(childSnap.val());
+//Delete
+function btnDeleteClicked(event){
 
-//     let recipe = childSnap.val();
-//     let $li = document.createElement('li');
-//     $li.innerHTML = recipe;
-//     $li.setAttribute('childKey', childSnap.key);
-//    // $li.addEventListener('click', recipeClicked)
-//     recipeList.appendChild($li);
+    event.stopPropagation();
 
-//     });
-// });
+    var recipeID = event.target.getAttribute('childKey');
+
+    const recipeRef = database.child('Recipe/' + recipeID);
+
+    recipeRef.remove();
+}
+
